69e5ff2253c361981cd154ca007ae9b23e5250e9
Md Shoaib Raza <razashoaib270@gmail.com>
2023-04-22 00:00:41 +0530

    Bottom NavBar created

--- app/src/main/java/com/example/ghumoo/HomePage.kt
+++ app/src/main/java/com/example/ghumoo/HomePage.kt
@@ -2,19 +2,46 @@ package com.example.ghumoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceControl.Transaction
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class HomePage : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-//        auth = Firebase.auth
-        findViewById<Button>(R.id.signout).setOnClickListener {
-            auth.signOut()
+        loadFragment(HomeFragment())
+        bottomNavigationView = findViewById(R.id.bottomNav)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.homeTab -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.searchTab -> {
+                    loadFragment(SearchFragment())
+                    true
+                }
+                R.id.userTab -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+            }
         }
+    }
+    private fun loadFragment(fragment:Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/ghumoo/LoginManager.kt
@@ -0,0 +1,22 @@
+package com.example.ghumoo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginManager : AppCompatActivity() {
+//    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_manager)
+        val user = Firebase.auth.currentUser
+        if(user != null){
+            startActivity(Intent(this,HomePage::class.java))
+        }else{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
