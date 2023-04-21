b4b6b67db4c083d28baf20ea9ab60e55cfd9139d
Md Shoaib Raza <razashoaib270@gmail.com>
2023-04-21 22:10:21 +0530

    Email Login and SignUp created

--- app/src/main/java/com/example/ghumoo/LoginFragment.kt
+++ app/src/main/java/com/example/ghumoo/LoginFragment.kt
@@ -1,16 +1,52 @@
 package com.example.ghumoo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class LoginFragment : Fragment() {
+    private lateinit var email : TextView
+    private lateinit var password :TextView
+    lateinit var loginBtn :Button
+    private lateinit var auth :FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_login, container, false)
+        email = v.findViewById(R.id.editTextTextEmailAddress)
+        password = v.findViewById(R.id.editTextTextPassword)
+        loginBtn = v.findViewById(R.id.Login)
+        auth = Firebase.auth
+        loginBtn.setOnClickListener { 
+            loginUser()
+        }
+        return v
+    }
+    private fun loginUser(){
+        val Email = email.text.toString()
+        val Password = password.text.toString()
+        if (Email.isEmpty() || Password.isEmpty()){
+            Toast.makeText(requireActivity(), "Email and Password can't be empty", Toast.LENGTH_SHORT).show()
+        }else{
+            auth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener{
+                if(it.isSuccessful){
+                    Toast.makeText(requireActivity(), "Login Was Successful", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(requireActivity(),HomePage::class.java))
+                }else{
+                    Toast.makeText(requireActivity(), "Invalid Credentials!!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ghumoo/SignUpFragment.kt
+++ app/src/main/java/com/example/ghumoo/SignUpFragment.kt
@@ -1,15 +1,62 @@
 package com.example.ghumoo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class SignUpFragment : Fragment() {
+    lateinit var username : EditText
+    lateinit var email : EditText
+    private lateinit var password : EditText
+    lateinit var signUpBtn : Button
+
+    //Initializing Firebase Auth
+    private lateinit var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val v : View = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        username = v.findViewById(R.id.editTextTextPersonName)
+        email = v.findViewById(R.id.editTextTextEmailAddress2)
+        password = v.findViewById(R.id.editTextTextPassword2)
+        signUpBtn = v.findViewById(R.id.signUpBtn)
+        //initializing the firebase auth object
+        auth = Firebase.auth
+
+        signUpBtn.setOnClickListener {
+            signUpUser()
+        }
+        return v
+    }
+    private fun signUpUser(){
+        val Email = email.text.toString()
+        val pass = password.text.toString()
+        // val uName = username.text.toString()
+        if (Email.isBlank() || pass.isBlank()){
+            Toast.makeText(requireActivity(),"Email and password can't be blank!!",Toast.LENGTH_SHORT).show()
+//            return
+        }else {
+            auth.createUserWithEmailAndPassword(Email, pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(requireActivity(), "Sign IN Successfully", Toast.LENGTH_SHORT)
+                        .show()
+                    startActivity(Intent(requireActivity(), HomePage::class.java))
+//                    finish()
+                } else {
+                    Toast.makeText(requireActivity(), "Sign IN Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
